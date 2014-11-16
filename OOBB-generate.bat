e3efd6945e0a2f9799d1ba00a4950565a7dc10d2
oomlout <aaron@oomlout.com>
2014-11-16 00:07:14 +0000

    added xml fixed some

--- OOBB-generate.bat
+++ OOBB-generate.bat
@@ -12,12 +12,34 @@ REM
 REM     Generating all part pages
 REM
 
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\BP\
 python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\CI\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\JA\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MM\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MP\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\PL\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SC\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SP\
+python %TPGEdirectory%TPGEmain.py -rm A -bd %OOBBpartsDirectory%\ -xa .oomp,xml -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp,C:\GH\oomlout-OOBB\part\OOBB-tag.xml -tm template/OOBB-template.tmpl.html -of %OOBBpartsDirectory%%%%%ID%%%%\%%%%ID%%%%-wiki.html
+
+
+
 
 REM
 REM      Generating Index Files
 REM
-
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\
 set extra=allParts
 	REM All Parts
-REM python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp -tm template/OOBB-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp -tm template/OOBB-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
+
+set extra=pictureIndex
+	REM All Parts
+python %TPGEdirectory%TPGEmain.py -bd %OOBBpartsDirectory% -xa .oomp -ex %OOBBpartsDirectory%,C:\GH\oomlout-OOBB\OOBB-helpers.oomp -tm template/OOBB-%extra%.tmpl.html -of %OOBBpartsDirectory%%extra%-wiki.html
--- OOBB-upload.bat
+++ OOBB-upload.bat
@@ -5,12 +5,27 @@ set WIKBdirectory=C:\GH\oomlout-WIKB\
 echo on
 
 
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\PL\
+
 REM
 REM 	UPLOADING PART PAGES
 REM
-
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\BP\
 python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\CI\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\JA\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MM\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MP\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\PL\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SC\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SP\
+python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
+
 
 echo off
 
@@ -18,7 +33,11 @@ echo off
 REM     UPLOADING Index Files
 
 	REM AllParts
-REM python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\GH\oomlout-OOBB\parts\allParts-wiki.html -wb projects/OOBB/allParts
+set extra=allParts
+python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\GH\oomlout-OOBB\parts\%extra%-wiki.html -wb projects/OOBB/%extra%
+
+set extra=pictureIndex
+python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\GH\oomlout-OOBB\parts\%extra%-wiki.html -wb projects/OOBB/%extra%
 
 
 
