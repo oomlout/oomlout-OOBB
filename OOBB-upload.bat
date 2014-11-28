e168fc9a5aa07b3100c73e37397c7dd5f884eb15
oomlout <aaron@oomlout.com>
2014-11-28 00:17:02 +0000

    Fixed upload so only requires one login

--- OOBB-upload.bat
+++ OOBB-upload.bat
@@ -7,37 +7,8 @@ echo on
 REM
 REM 	UPLOADING PART PAGES
 REM
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\BP\
+set extra1=allParts
+set extra2=pictureIndex
+
+set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\BP\,C:\GH\oomlout-OOBB\part\CI\,C:\GH\oomlout-OOBB\part\JA\,C:\GH\oomlout-OOBB\part\MM\,C:\GH\oomlout-OOBB\part\MP\,C:\GH\oomlout-OOBB\part\PL\,C:\GH\oomlout-OOBB\part\SC\,C:\GH\oomlout-OOBB\part\SP\,C:\GH\oomlout-OOBB\part\%extra1%-wiki.html,C:\GH\oomlout-OOBB\part\%extra2%-wiki.html
 python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\CI\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\JA\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MM\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\MP\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\PL\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SC\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-set OOBBpartsDirectory=C:\GH\oomlout-OOBB\part\SP\
-python %WIKBdirectory%WIKBmain.py -rm A -si oomlout -bd %OOBBpartsDirectory% -wb projects/oobb/part/ -fa '-wiki.html'
-
-
-echo off
-
-
-
-
-REM     UPLOADING Index Files
-
-	REM AllParts
-set extra=allParts
-python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\GH\oomlout-OOBB\part\%extra%-wiki.html -wb Projects/oobb/%extra%
-
-set extra=pictureIndex
-python %WIKBdirectory%WIKBmain.py -si oomlout -bd C:\GH\oomlout-OOBB\part\%extra%-wiki.html -wb Projects/oobb/%extra%
-
-
-
