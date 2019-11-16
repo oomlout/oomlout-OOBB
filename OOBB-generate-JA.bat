31380b4e202cf201f2200f759ee533acd54999bc
Oom Lout <info@oomlout.com>
2019-11-16 21:49:49 +0000

    Up to date generation.

--- OOBB-generate-JA.bat
+++ OOBB-generate-JA.bat
@@ -14,15 +14,16 @@ SET FILEFULL3DPR=%DIRECTORY%%FILE3DPR%
 SET FILEFULLMD=%DIRECTORY%%FILEMD%
 
 SET HEIGHT=01
+SET NAME=JA-%WIDTH%
 
 mkdir %DIRECTORY%
 
 SET MODE=JA3D
 SET STYLE=3DPR
 del %FILEFULL%.stl
-REM openscad -o %FILEFULL%.stl -D "w=%WIDTH%;h=%HEIGHT%;m=\"%MODE%\""  OOBB-generate.scad
+openscad -o %FILEFULL%.stl -D "w=%WIDTH%;h=%HEIGHT%;m=\"%MODE%\""  OOBB-generate.scad
 del %FILEFULL3DPR%.stl
-REM openscad -o %FILEFULL3DPR%.stl -D "w=%WIDTH%;h=%HEIGHT%;m=\"%MODE%\"" OOBB-generate3DPR.scad
+openscad -o %FILEFULL3DPR%.stl -D "w=%WIDTH%;h=%HEIGHT%;m=\"%MODE%\"" OOBB-generate3DPR.scad
 del %FILEFULL%.png
 openscad -o %FILEFULL%.png -D "w=%WIDTH%;h=%HEIGHT%;m=\"%MODE%\"" --render --colorscheme OOBB OOBB-generate.scad
 
