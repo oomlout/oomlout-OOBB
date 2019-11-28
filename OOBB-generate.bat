042b2c0fa50b4eb5f2cc1d9a1cb116162a2fac11
Oom Lout <info@oomlout.com>
2019-11-28 17:59:24 +0000

    Added more JA sizes.

--- OOBB-generate.bat
+++ OOBB-generate.bat
@@ -8,7 +8,16 @@ CALL OOBB-generate-NICHE.bat
 SET WIDTH=03
 CALL OOBB-generate-JA.bat
 
+SET WIDTH=04
+CALL OOBB-generate-JA.bat
+
 SET WIDTH=05
+CALL OOBB-generate-JA.bat
+
+SET WIDTH=06
+CALL OOBB-generate-JA.bat
+
+SET WIDTH=07
 CALL OOBB-generate-JA.bat
 
 
